05ac2d92318c9ee62676e3a9019272e153b7f5a5
Manishbgst <68889128+Manishbgst@users.noreply.github.com>
2020-12-14 00:30:32 +0530

    Add initial bd files

---
+++ main.js
@@ -0,0 +1,12 @@
+
+<!--hide
+var password;
+var pass="nandu";
+password=prompt('tell me your fav nickname i call u','');
+if(password==pass)
+alert('happy bdy fatty.');
+else
+ {
+  window.location="birth.html";
+  }
+  //-->
